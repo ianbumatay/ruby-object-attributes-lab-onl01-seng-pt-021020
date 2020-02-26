9af7357ccd4eda10995c508f4788dafb7f6eef53
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-26 13:56:28 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  def name= ( name)
+    @name = name
+   end
+
+   def name
+     @name
+   end
+
+
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def job
+    @breed
+  end
+
+end
